390e03694ecfc1a23b50db3cb1fc61556931cf43
Делендик Иван Валерьевич <idelendik@notary.bel>
2020-11-11 18:01:49 +0300

    Add Home Work A

--- HomeWork2/app.js
+++ HomeWork2/app.js
@@ -1,3 +1,36 @@
-for(let i = 1; i<=10; i++) {
-    console.log(i)
+let data = [
+    {
+        firstName: 'Ashton',
+        lastName: 'Kutcher',
+        age: 40
+    }, 
+    {
+        firstName: 'Bradley',
+        lastName: 'Pitt',
+        age: 54
+    }, 
+    {
+        firstName: 'Hannah',
+        lastName: 'Dakota',
+        age: 24
+    }
+]; 
+
+const inputLastName = prompt('Please enter your last name');
+
+let dateSearch = '';
+
+for ( i=0; i < data.length; i++) {
+    if (`${inputLastName}` === data[i].lastName) {
+        dateSearch = data[i];
+    }    
 }
+
+if (`${dateSearch.lastName}` === `${inputLastName}`) {
+    
+    alert(` user name: ${dateSearch.firstName} ${dateSearch.lastName}. \n user age: ${dateSearch.age}. `);
+}
+    else { 
+        alert('No results found for your request')
+    }
+
