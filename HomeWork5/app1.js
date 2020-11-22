1c6e58468da30ef0044410d3b6598a9136369224
Delendik Ivan <vanya.delya@mail.ru>
2020-11-22 17:27:11 +0300

    debag feirst task

--- HomeWork5/app1.js
+++ HomeWork5/app1.js
@@ -1,7 +1,7 @@
 const inputWord = prompt("Введите слово для проверки на полиндром");
 const testWord = inputWord.split("").reverse().join("");
 
-if (inputWord === testWord) {
+if (inputWord.toLowerCase() === testWord.toLowerCase()) {
   alert("Слово является полиндромом!");
 } else {
   alert("Слово не является полиндромом!");
