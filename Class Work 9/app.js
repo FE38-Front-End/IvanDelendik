33482475a52840892acf37ad39abce783e26a950
IvanDelendik <vanya.delya@mail.ru>
2020-12-26 18:22:27 +0300

    Добавление input, localStorage

--- Class Work 9/app.js
+++ Class Work 9/app.js
@@ -10,97 +10,137 @@ const buttonSaveCircle = document.getElementById("button-save-circle");
 const inputSaveCircle = document.getElementById("input-save-circle");
 const buttonSaveStep = document.getElementById("button-save-step");
 const inputSaveStep = document.getElementById("input-save-step");
+const buttonsBlock = document.querySelector(".buttons");
+const buttonsBlockSave = document.querySelector(".parametrs");
 
-buttonSaveSquare.addEventListener("click", () => {
-  if (+inputSaveCircle.value >= +inputSaveSquare.value) {
-    alert(
-      "Ошибка, невозожно вписать круг по заданным параметрам. Измените диаметр окружности или длину квадрата"
-    );
-  } else {
-    square.style.width = inputSaveSquare.value + "px";
-    square.style.height = inputSaveSquare.value + "px";
-    square.style.display = "block";
-  }
-});
+buttonUp.disabled = true;
+buttonLeft.disabled = true;
+buttonRight.disabled = true;
+buttonDown.disabled = true;
+buttonSaveCircle.disabled = true;
+buttonSaveStep.disabled = true;
 
-buttonSaveCircle.addEventListener("click", () => {
-  if (+inputSaveCircle.value >= +inputSaveSquare.value) {
-    alert(
-      "Ошибка, невозожно вписать круг по заданным параметрам. Измените диаметр окружности или длину квадрата"
-    );
-  } else {
-    circle.style.width = inputSaveCircle.value + "px";
-    circle.style.height = inputSaveCircle.value + "px";
+function loacalSave() {
+  inputSaveSquare.value = localStorage.getItem("localSquare");
+  inputSaveCircle.value = localStorage.getItem("localCircle");
+  inputSaveStep.value = localStorage.getItem("localStep");
+}
+
+loacalSave();
+
+buttonsBlockSave.addEventListener("click", (e) => {
+  const buttonTypeSave = e.target.dataset.type;
+
+  if (buttonTypeSave) {
     topIndent = +(inputSaveSquare.value / 2 - inputSaveCircle.value / 2);
     circle.style.marginTop = topIndent + "px";
     leftIndent = +(inputSaveSquare.value / 2 - inputSaveCircle.value / 2);
     circle.style.marginLeft = leftIndent + "px";
-    circle.style.display = "block";
+
+    step = +inputSaveStep.value;
+    if (topIndent >= step) {
+      buttonUp.disabled = false;
+      buttonLeft.disabled = false;
+      buttonRight.disabled = false;
+      buttonDown.disabled = false;
+    } else {
+      buttonUp.disabled = true;
+      buttonLeft.disabled = true;
+      buttonRight.disabled = true;
+      buttonDown.disabled = true;
+    }
+
+    switch (buttonTypeSave) {
+      case "button-save-square":
+        if (+inputSaveCircle.value >= +inputSaveSquare.value) {
+          alert(
+            "Ошибка, невозожно вписать круг по заданным параметрам. Измените диаметр окружности или длину квадрата"
+          );
+        } else {
+          square.style.width = inputSaveSquare.value + "px";
+          square.style.height = inputSaveSquare.value + "px";
+          square.style.display = "block";
+          localStorage.setItem("localSquare", inputSaveSquare.value);
+          buttonSaveCircle.disabled = false;
+        }
+        break;
+
+      case "button-save-circle":
+        if (+inputSaveCircle.value >= +inputSaveSquare.value) {
+          alert(
+            "Ошибка, невозожно вписать круг по заданным параметрам. Измените диаметр окружности или длину квадрата"
+          );
+        } else {
+          circle.style.width = inputSaveCircle.value + "px";
+          circle.style.height = inputSaveCircle.value + "px";
+          localStorage.setItem("localCircle", inputSaveCircle.value);
+          circle.style.display = "block";
+          buttonSaveStep.disabled = false;
+        }
+        break;
+
+      case "button-save-step":
+        step = +inputSaveStep.value;
+        localStorage.setItem("localStep", inputSaveStep.value);
+        if (topIndent >= step) {
+          buttonUp.disabled = false;
+          buttonLeft.disabled = false;
+          buttonRight.disabled = false;
+          buttonDown.disabled = false;
+        }
+        break;
+    }
   }
 });
 
-buttonSaveStep.addEventListener("click", () => {
-  step = +inputSaveStep.value;
-  if (topIndent < step) {
-    buttonUp.disabled = true;
-    buttonLeft.disabled = true;
-    buttonRight.disabled = true;
-    buttonDown.disabled = true;
-  }
-});
+buttonsBlock.addEventListener("click", (e) => {
+  const buttonType = e.target.dataset.type;
 
-buttonUp.addEventListener("click", () => {
-  if (topIndent - step >= 0) {
-    circle.style.marginTop = topIndent - step + "px";
-    topIndent = topIndent - step;
-    buttonDown.disabled = false;
-  }
-});
+  if (buttonType) {
+    switch (buttonType) {
+      case "button-up":
+        if (topIndent - step >= 0) {
+          circle.style.marginTop = topIndent - step + "px";
+          topIndent = topIndent - step;
+          buttonDown.disabled = false;
+        }
+        if (topIndent < step) {
+          buttonUp.disabled = true;
+        }
+        break;
 
-buttonUp.addEventListener("click", () => {
-  if (topIndent < step) {
-    buttonUp.disabled = true;
-  }
-});
+      case "button-down":
+        if (topIndent < inputSaveSquare.value - inputSaveCircle.value) {
+          circle.style.marginTop = topIndent + step + "px";
+          topIndent = topIndent + step;
+          buttonUp.disabled = false;
+        }
+        if (topIndent + step > inputSaveSquare.value - inputSaveCircle.value) {
+          buttonDown.disabled = true;
+        }
+        break;
 
-buttonDown.addEventListener("click", () => {
-  if (topIndent < inputSaveSquare.value - inputSaveCircle.value) {
-    circle.style.marginTop = topIndent + step + "px";
-    topIndent = topIndent + step;
-    buttonUp.disabled = false;
-  }
-});
+      case "button-left":
+        if (leftIndent - step >= 0) {
+          circle.style.marginLeft = leftIndent - step + "px";
+          leftIndent = leftIndent - step;
+          buttonRight.disabled = false;
+        }
+        if (leftIndent < step) {
+          buttonLeft.disabled = true;
+        }
+        break;
 
-buttonDown.addEventListener("click", () => {
-  if (topIndent + step > inputSaveSquare.value - inputSaveCircle.value) {
-    buttonDown.disabled = true;
-  }
-});
-
-buttonLeft.addEventListener("click", () => {
-  if (leftIndent - step >= 0) {
-    circle.style.marginLeft = leftIndent - step + "px";
-    leftIndent = leftIndent - step;
-    buttonRight.disabled = false;
-  }
-});
-
-buttonLeft.addEventListener("click", () => {
-  if (leftIndent < step) {
-    buttonLeft.disabled = true;
-  }
-});
-
-buttonRight.addEventListener("click", () => {
-  if (leftIndent < inputSaveSquare.value - inputSaveCircle.value) {
-    circle.style.marginLeft = leftIndent + step + "px";
-    leftIndent = leftIndent + step;
-    buttonLeft.disabled = false;
-  }
-});
-
-buttonRight.addEventListener("click", () => {
-  if (leftIndent + step > inputSaveSquare.value - inputSaveCircle.value) {
-    buttonRight.disabled = true;
+      case "button-right":
+        if (leftIndent < inputSaveSquare.value - inputSaveCircle.value) {
+          circle.style.marginLeft = leftIndent + step + "px";
+          leftIndent = leftIndent + step;
+          buttonLeft.disabled = false;
+        }
+        if (leftIndent + step > inputSaveSquare.value - inputSaveCircle.value) {
+          buttonRight.disabled = true;
+        }
+        break;
+    }
   }
 });
