7805c17e15bd7a016a37490e3c7e5d2c039c5c1c
Delendik Ivan <vanya.delya@mail.ru>
2021-02-16 19:19:00 +0300

    WebPack

---
+++ WebPack_Shop/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|otf|jpg|jpeg)$/,
+        use: "url-loader?limit=100000",
+      },
+    ],
+  },
+};
